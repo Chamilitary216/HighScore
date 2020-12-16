ce0abd650b32a1d01fa53c58bb1796ddbc59a18a
Chris Curry <Chamilitary216@hotmail.com>
2020-12-16 00:02:27 -0500

    psuedo coded and added all js files needed for code

---
+++ assets/js/psuedoCode.js
@@ -0,0 +1,9 @@
+//this quiz will need a list of questions.
+
+//When the button Start Quiz is pressed it will present a series of random questions.
+//When the button Start Quiz is pressed it will start a timer count down.
+
+//Each wrong answer the tiner will decrease by 10 seconds from the current second followed by "Wrong Answer" text. 
+//Each right answer will add 1+ to the score followed by "Right Answer" text.
+
+//
