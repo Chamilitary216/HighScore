d63be2b6f9262c9df1b5567bc29bdf26e1e9d4e1
Chris Curry <Chamilitary216@hotmail.com>
2020-12-17 02:01:24 -0500

    fixed errors with questions, missing commas

---
+++ assets/js/logic.js
@@ -0,0 +1,18 @@
+//defining variables
+
+var score
+
+var startBtn = document.getElementById("startBtn");
+var submitBtn = document.getElementById("submitBtn");
+var userScoreEl = document.getElementById("userScore");
+var questionsEl = document.getElementById("questions");
+var submitScoreEl = document.getElementById("#submit-score");
+var answerChoices = document.getElementById("answers")
+var timerEl = document.getElementById("timer")
+var secondsLeft = (questions.length)
+var answer;
+var questioNumber = -1;
+
+
+
+
---
+++ assets/js/questions.js
@@ -0,0 +1,23 @@
+var questions = [
+    
+    
+    {   // question statement 
+        title: "What does API stand for?",
+        //multiple choice answers
+        choices: ["Application Programming Inferface", "American Programming Institution", "All Pro Index", "Automated Processing Interface"],
+        //the actual answer for the question
+        answer: "Application Programming Inferface"
+    },
+
+    {
+        title: "Common generic placeholder paragraph text is called...",
+        choices: ["Pon de River", "Lorem Ipsum", "Ipsum Lorem", "Epson Salt"],
+        answer: "Loren Ipsum"
+    },
+
+    {
+        title: "Full Stack Web Developers can develop...",
+        choices: ["Client Software", "Sever Software", "Virus Software", "Both A & B"],
+        answer: "Both A & B"
+    }
+]
